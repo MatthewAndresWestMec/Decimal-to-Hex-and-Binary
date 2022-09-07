f8f9709ec26294bd4b857529c6039705d5251ca6
MatthewAndresWestMec <111930814+MatthewAndresWestMec@users.noreply.github.com>
2022-09-06 17:19:35 -0700

    renamed script and added alert for answers

--- DecBinHex.js
+++ DecBinHex.js
@@ -21,35 +21,45 @@ if (e.length<8)
 if(l == -7)
 {
     console.log(`0000000 ${n.toString(2)}`);
+    alert(`0000000 ${n.toString(2)}`);
 }
 else if(l == -6)
 {
     console.log(`000000${n.toString(2)}`);
+    alert(`000000${n.toString(2)}`);
 }
 else if(l == -5)
 {
     console.log(`00000${n.toString(2)}`);
+    alert(`00000${n.toString(2)}`);
 }
 else if(l == -4)
 {
     console.log(`0000${n.toString(2)}`);
+    alert(`0000${n.toString(2)}`);
 }
 else if(l == -3)
 {
     console.log(`000${n.toString(2)}`);
+    alert(`000${n.toString(2)}`);
 }
 else if(l == -2)
 {
     console.log(`00${n.toString(2)}`);
+    alert(`00${n.toString(2)}`);
 }
 else if(l == -1)
 {
-    console.log(`0${n.toString(2)}`);;
+    console.log(`0${n.toString(2)}`);
+    alert(`0${n.toString(2)}`);
 }
 else
 {
     console.log(`${Number(n).toString(2)}`);
+    alert(`${Number(n).toString(2)}`);
 }
+
+
 
 //decimal to hex using .toString
 console.log(Math.abs(n).toString(16));
