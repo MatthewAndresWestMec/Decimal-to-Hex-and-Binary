4b918f61ae78613935f71d8ce3e57a61ef252824
MatthewAndresWestMec <111930814+MatthewAndresWestMec@users.noreply.github.com>
2022-09-06 17:09:57 -0700

    updated dec to hex

--- BinHex.js
+++ BinHex.js
@@ -1,3 +1,6 @@
+//prompt for number
 let n = prompt ("What is your decimal")
+
+//decimal to hex using .toString
 console.log(Math.abs(n).toString(16));
-alert(Math.abs(n).toString(16));
+alert(Math.abs(n).toString(16)); 
