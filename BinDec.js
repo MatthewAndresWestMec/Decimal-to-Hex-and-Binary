4802f00b635c03fe490b0cce3268c9b5ca66cc71
Tyler <111825114+WeezerFan096@users.noreply.github.com>
2022-09-06 17:10:44 -0700

    math
    does it

---
+++ BinDec.js
@@ -0,0 +1,52 @@
+//defines all the vars we need to do math later
+
+let val = prompt("what is the decimal?");
+var l;
+var e;
+let n = (Number(val).toString(2)) 
+e = String(n)
+
+//converts the decimal to unformatted bianry
+
+if (e.length<8)
+{
+    l = e.length
+    l = l - 8;
+}
+
+
+
+// adds the zeros to the front of the bianry number
+
+if(l == -7)
+{
+    console.log(`0000000 ${n.toString(2)}`);
+}
+else if(l == -6)
+{
+    console.log(`000000${n.toString(2)}`);
+}
+else if(l == -5)
+{
+    console.log(`00000${n.toString(2)}`);
+}
+else if(l == -4)
+{
+    console.log(`0000${n.toString(2)}`);
+}
+else if(l == -3)
+{
+    console.log(`000${n.toString(2)}`);
+}
+else if(l == -2)
+{
+    console.log(`00${n.toString(2)}`);
+}
+else if(l == -1)
+{
+    console.log(`0${n.toString(2)}`);;
+}
+else
+{
+    console.log(`${Number(n).toString(2)}`);
+}
--- BinHexDec.js
+++
@@ -1,6 +0,0 @@
-
-let n = prompt("what is the decimal?");
-console.log(Number(n).toString(2));
-    alert(Number(n).toString(2));
-console.log(Math.abs(n).toString(16));
-    alert(Math.abs(n).toString(16));
